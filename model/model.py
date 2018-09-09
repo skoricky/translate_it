d919d8eb92c0e43813117543615ff3621fbf9b8e
pripisnov <pripisnov@bk.ru>
2018-09-09 03:22:55 +0300

    model+

--- model/model.py
+++ model/model.py
@@ -1,58 +1,85 @@
 import json
-from abc import ABCMeta, abstractmethod, abstractproperty
+from abc import ABCMeta, abstractmethod
 
 
 class AbsModel:
     """
     Абстрактный класс для описания стандартных методов для моделей
     """
+
+    def __init__(self, file_name=None, data=None):
+        self.file_name = file_name
+        self.data = data
+
     __metaclass__ = ABCMeta
 
     @abstractmethod
-    def get_blocks(self):
+    def create_project(self, project_name: str):
         pass
 
     @abstractmethod
-    def set_blocks(self):
+    def open_project(self, project_name: str):
         pass
 
     @abstractmethod
+    def get_data(self):
+        pass
+
+    @abstractmethod
+    def set_data(self):
+        pass
+
     def dumpto_file(self):
-        pass
-		
+        try:
+            with open(self.file_name, 'w') as file:
+                file.write(self.data)
+        except Exception as e:
+            print('Error (so bad):', e)
+
+    def loadfrom_file(self):
+        try:
+            with open(self.file_name, 'r') as file:
+                self.data = file.read()
+            return self.data
+        except Exception as e:
+            print('Error (so bad):', e)
+
 
 class SQLiteRepository(AbsModel):
-	def __init__(self, file_name,data):
-		self.file_name = file_name
-		self.data = data
-		
-	def create_project(self, projet_name):
-		pass
-		
-	def get_data(self):
-		pass
-		
-	def set_data(self):
-		pass
-		
-	def dumpto_file(self):
-		try:
-			with open(self.file_name, "r") as file:
-				file.write(self.data)
-			return True	
-		except Exception as e:
-			print('Error (so bad):', e)
-
-
-class TempRepository:
 
     __slots__ = ['file_name', 'data']
 
     def __init__(self, file_name, data):
-        self.file_name = file_name
-        self.data = data
+        super().__init__(file_name, data)
 
-    def dumps_tojson(self):
+    def create_project(self, project_name: str):
+        pass
+
+    def open_project(self, project_name: str):
+        pass
+
+    def get_data(self):
+        pass
+
+    def set_data(self):
+        pass
+
+
+class TempRepository(AbsModel):
+
+    __slots__ = ['file_name', 'data']
+
+    def __init__(self, file_name, data):
+        super().__init__(file_name, data)
+
+    def create_project(self, project_name):
+        pass
+
+    def open_project(self, project_name: str):
+        return self.get_data()
+
+    def set_data(self):
+        print(self.file_name, self.data)
         try:
             with open(self.file_name, 'w') as file:
                 file.write(json.dumps(self.data))
@@ -60,7 +87,7 @@ class TempRepository:
         except Exception as e:
             print('Error (so bad):', e)
 
-    def loads_fromjson(self):
+    def get_data(self):
         try:
             with open(self.file_name, 'r') as file:
                 data = dict(json.loads(file.read(), encoding='utf-8'))
@@ -74,5 +101,5 @@ class Repository(TempRepository):
 
     __slots__ = ['file_name', 'data']
 
-    def __init__(self, file_name=None, data=None):
+    def __init__(self, file_name='Hello.json', data=None):
         super().__init__(file_name, data)
